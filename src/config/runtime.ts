432b6ff5adf0d4ed1d397e99e6f3b5fef7f995a3
= <carroll.landry@gmail.com>
2026-01-25 16:14:29 -0500

    config: remove workspace.yaml

--- src/config/runtime.ts
+++ src/config/runtime.ts
@@ -1,8 +1,5 @@
 import { FetchHttpClient } from "@effect/platform";
-import { NodeFileSystem } from "@effect/platform-node";
-import { Layer, ManagedRuntime } from "effect";
+import { ManagedRuntime } from "effect";
 
 export const ApiRuntime = ManagedRuntime.make(FetchHttpClient.layer);
-export const ApiTestRuntime = ManagedRuntime.make(
-  Layer.merge(FetchHttpClient.layer, NodeFileSystem.layer),
-);
+export const ApiTestRuntime = ManagedRuntime.make(FetchHttpClient.layer);
